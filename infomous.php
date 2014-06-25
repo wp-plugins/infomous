670bca536cc49697b303cdbfc2a67cfd3a4f37aa
Dualcube <Dualcube@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-25 07:10:43 +0000

    fixed compatibility issue
    git-svn-id: https://plugins.svn.wordpress.org/infomous/trunk@938291 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- infomous.php
+++ infomous.php
@@ -4,7 +4,7 @@ Plugin Name: Infomous
 Plugin URI: http://dualcube.com/
 Description: Infomous is the interactive word cloud that enhances your site by helping visitors find relevant content from your site or from any other source you choose. Add Infomous to a specific page to showcase related information, or place it on your entire site to facilitate navigation. Use your own content, or cruise to infomous.com to create a cloud from social media, news, web searches or any other content that you want to feature.
 Author: Dualcube
-Version: 1.0.3
+Version: 1.0.4
 Author URI: http://dualcube.com/
 */
 
